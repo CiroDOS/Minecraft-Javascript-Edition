0ea0664b2227f429fdeb1e0f5c62dcf870a1875a
CiroDOS <cirod307@gmail.com>
2023-04-05 11:26:03 +0200

    Game Experience

---
+++ Gameplay.js
@@ -0,0 +1,14 @@
+/**
+ * Gameplay.info.emulator-url is the minecraft gameplay
+ * is a web page embedded in a iframe that contains gameplay.
+ * If you found a best html gameplay, please input it's url in
+ * the variable beta_gameplay
+ */
+
+const beta_gameplay = ''; // Not beta gameplay url :'(
+
+class Gameplay {
+    static info = {
+        'emulator-url': "https://minecraft-javascript-edition--systemdrive.repl.co/"
+    };
+}
